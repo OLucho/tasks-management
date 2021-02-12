19b4540aec0451a70370295e6c920b4c2d62e152
Olucho <luchoorfino@gmail.com>
2021-02-12 20:17:58 -0300

    Adds JWT & Passport config on auth module

--- backend-nestjs/src/auth/auth.module.ts
+++ backend-nestjs/src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600, // 1hour
+      },
+    }),
+    TypeOrmModule.forFeature([AuthRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
