9dafc92948a74075a003878d51af88506e66cdac
Olucho <luchoorfino@gmail.com>
2021-02-12 19:23:51 -0300

    Add user SignUp funcionality

--- backend-nestjs/src/auth/auth.repository.ts
+++ backend-nestjs/src/auth/auth.repository.ts
@@ -1,5 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class AuthRepository extends Repository<User> {}
+export class AuthRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
+}
--- backend-nestjs/src/auth/auth.service.ts
+++ backend-nestjs/src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthRepository } from './auth.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(AuthRepository) private authRepository: AuthRepository,
   ) {}
+
+  signUp(authCredentialsDto: AuthCredentialsDto) {
+    return this.authRepository.signUp(authCredentialsDto);
+  }
 }
