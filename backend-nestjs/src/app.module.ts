408c28207f62e1dd16ebc60d6d9fb8adea3172d2
Olucho <luchoorfino@gmail.com>
2021-02-12 20:24:25 -0300

    Injects Auth Module in app

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -4,8 +4,9 @@ dotenv.config();
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
