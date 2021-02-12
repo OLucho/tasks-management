044b728d087ba04bd63fa78c6856b6831b2d836e
Olucho <luchoorfino@gmail.com>
2021-02-12 19:47:25 -0300

    Adds Bcrypt and hash users password

--- backend-nestjs/src/auth/auth.repository.ts
+++ backend-nestjs/src/auth/auth.repository.ts
@@ -5,14 +5,18 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class AuthRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.salt();
+    user.password = await this.hashPassword(password, user.salt);
+
     try {
       await user.save();
     } catch (error) {
@@ -23,5 +27,9 @@ export class AuthRepository extends Repository<User> {
         throw new InternalServerErrorException('Wops   Something went wrong');
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt);
   }
 }
