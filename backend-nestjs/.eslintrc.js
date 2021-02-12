080924dc6228114eed4b85d273f838f4bb67fc68
Olucho <luchoorfino@gmail.com>
2021-02-12 12:22:06 -0300

    Add Tasks Module and Controller

--- backend-nestjs/.eslintrc.js
+++ backend-nestjs/.eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
