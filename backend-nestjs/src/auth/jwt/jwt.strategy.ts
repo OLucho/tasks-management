9d693f2f3bfae66eab17fc6c8f29f95e50be2866
Olucho <luchoorfino@gmail.com>
2021-02-13 11:07:56 -0300

    Fix bug

--- backend-nestjs/src/auth/jwt/jwt.strategy.ts
+++ backend-nestjs/src/auth/jwt/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
   async validate(payload: JwtPayload) {
     const { username } = payload;
-    const user = await this.userRepository.findOne(username);
+    const user = await this.userRepository.findOne({ username });
 
     if (!user) {
       throw new UnauthorizedException();
