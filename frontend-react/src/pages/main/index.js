a24d066a8cfd007922c0bd28c4346fe5868eb471
Olucho <luchoorfino@gmail.com>
2021-02-14 14:08:30 -0300

    First styles to main page

--- frontend-react/src/pages/main/index.js
+++ frontend-react/src/pages/main/index.js
@@ -1,3 +1,25 @@
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
+import { useAuth } from "../../hooks/auth";
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 export default function Main() {
-  return <div>hello from main</div>;
+  const classes = useStyles();
+  const { user } = useAuth();
+  return (
+    <Box display="flex" justifyContent="space-evenly">
+      <div>
+        <h1>Tasks Managment</h1>
+        <span>Hello ${user.username}</span>
+      </div>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Create New Task
+      </Button>
+    </Box>
+  );
 }
