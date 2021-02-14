27f832528314d9b786fbc2e6e0bbdee656c6afbd
Olucho <luchoorfino@gmail.com>
2021-02-14 11:00:06 -0300

    Auth method to send user data

--- backend-nestjs/src/auth/auth.controller.ts
+++ backend-nestjs/src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorator';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +27,16 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/user')
+  @UseGuards(AuthGuard())
+  getUser(@GetUser() user: User) {
+    const userData = {
+      username: user.username,
+      id: user.id,
+      tasks: user.tasks,
+    };
+    return userData;
   }
 }
