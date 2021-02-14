9b1cb60b829eeed4d010dca80ce9fea882504678
Olucho <luchoorfino@gmail.com>
2021-02-14 18:00:49 -0300

    Adds tasks filter component

---
+++ frontend-react/src/components/tasks/task-filter.js
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Container,
+  FormControl,
+  InputAdornment,
+  MenuItem,
+  Select,
+  TextField,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { useState } from "react";
+
+export default function TasksFilter() {
+  const [status, setStatus] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleFilters = async (status, search) => {
+    console.log(status, search);
+  };
+
+  return (
+    <Container style={{ textAlign: "center", margin: "2rem 0" }}>
+      <FormControl style={{ width: "220px", margin: "0 3rem" }}>
+        <Select
+          displayEmpty
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <MenuItem value="">No status filter</MenuItem>
+          <MenuItem value={"OPEN"}>Open</MenuItem>
+          <MenuItem value={"IN_PROGRESS"}>In Progress</MenuItem>
+          <MenuItem value={"DONE"}>Done</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl style={{ width: "220px" }}>
+        <TextField
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search..."
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormControl>
+      <Button
+        variant="outlined"
+        color="primary"
+        style={{ marginLeft: "1rem" }}
+        onClick={() => handleFilters(status, search)}
+      >
+        Filter
+      </Button>
+    </Container>
+  );
+}
