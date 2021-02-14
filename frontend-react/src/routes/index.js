ffe3d2dd6a797db335d456e6a40ab1e48809c2f1
Olucho <luchoorfino@gmail.com>
2021-02-14 12:25:10 -0300

    Adds main page

--- frontend-react/src/routes/index.js
+++ frontend-react/src/routes/index.js
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Route from "./handleRoutes";
 import signIn from "../pages/signIn";
 import SignUp from "../pages/signUp";
+import Main from "../pages/main";
+
 export default function Routes() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={signIn} />
-          <Route path="/signin" component={signIn} />
-          <Route path="/signup" component={SignUp} />
+          <Route exact path="/" component={Main} isPrivate />
+          <Route exact path="/signin" component={signIn} />
+          <Route exact path="/signup" component={SignUp} />
         </Switch>
       </Router>
     </>
