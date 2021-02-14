fe62903fc2d5dbccc5570e6c60fef700120e9e71
Olucho <luchoorfino@gmail.com>
2021-02-14 15:25:37 -0300

    Add GlobalStyles

---
+++ frontend-react/src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    html{
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+
+`;
